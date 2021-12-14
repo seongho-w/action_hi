3b3fd46cab99f3db2e7ee0c3c9ac8f54c8fd617f
seongho-w <sol0632@naver.com>
2021-12-15 03:28:28 +0900

    #1

---
+++ src/main/java/com/weling/ActionHiApplication.java
@@ -0,0 +1,13 @@
+package com.weling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionHiApplication.class, args);
+    }
+
+}
