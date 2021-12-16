19bf905548550b96d34abcbfcbe2f110458b9153
jtheeeeee <j_03_17@naver.com>
2021-12-16 12:09:06 +0900

    CORS

--- src/main/java/com/weling/controller/TestController.java
+++ src/main/java/com/weling/controller/TestController.java
@@ -1,9 +1,10 @@
 package com.weling.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "https://api.weling.site")
 @RestController
 public class TestController {
 
