d68e9a95a91bc8432ecf1045c202c4075960b81a
jtheeeeee <j_03_17@naver.com>
2021-12-16 16:58:23 +0900

    commit

--- src/main/java/com/weling/controller/TestController.java
+++ src/main/java/com/weling/controller/TestController.java
@@ -1,13 +1,31 @@
 package com.weling.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.weling.dto.JwtResponse;
+import com.weling.dto.SignupRequestDto;
+import com.weling.service.UserService;
+import com.weling.util.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+
 @CrossOrigin(origins = "https://www.weling.site/")
+@RequiredArgsConstructor
 @RestController
 public class TestController {
 
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final UserService userService;
 
     @GetMapping("/test")
     public String test(){
@@ -17,10 +35,24 @@ public class TestController {
         return testStr;
     }
 
-    @GetMapping("/signup")
-    public String signup(){
-        return "hi";
+    @PostMapping(value = "/signup")
+    public ResponseEntity<?> createUser(@RequestBody SignupRequestDto userDto) throws Exception {
+        System.out.println("회원가입");
+        userService.registerUser(userDto);
+        authenticate(userDto.getUsername(), userDto.getPassword());
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(userDto.getUsername());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token, userDetails.getUsername()));
+    }
 
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
     }
 
 }
